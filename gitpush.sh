fa999416b9266b19c1d10bf3589c245a45dacbb7
sxuet <1065464173@qq.com>
2022-03-07 17:19:02 +0800

    autocommit

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #description:将halo博客的数据备份到github上
 
-echo "===========================$(date +\%Y\%m\%d-\%H\%M)===============================" >> '/home/halo/halo_data.bak/gitpush.log'
+echo "===========================$(date +\%Y-\%m-\%d-\%H:\%M)===============================" >> '/home/halo/halo_data.bak/gitpush.log'
 
 git -C '/home/halo/halo_data.bak' add . >> '/home/halo/halo_data.bak/gitpush.log' 2>&1
 git -C '/home/halo/halo_data.bak' commit -m "autocommit" >> '/home/halo/halo_data.bak/gitpush.log' 2>&1
