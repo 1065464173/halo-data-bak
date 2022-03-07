65414464547e872ddf5758ce868b4e0a8b0bcd5b
sxuet <1065464173@qq.com>
2022-03-07 17:06:16 +0800

    autocommit

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #description:将halo博客的数据备份到github上
 
-git -C '/home/halo/halo_data.bak' add .
+git -C '/home/halo/halo_data.bak' add . 
 git -C '/home/halo/halo_data.bak' commit -m "autocommit"
-git -C '/home/halo/halo_data.bak' push origin main
+git -C '/home/halo/halo_data.bak' push origin main >> '/home/halo/halo_data.bak/gitpush.log' 2>&1
